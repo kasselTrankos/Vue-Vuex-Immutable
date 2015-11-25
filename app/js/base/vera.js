9ff69992a274ad12805c709843f1b9c9f751fdae
kasselTrankos <alvaro.touzon@gmail.com>
2015-11-25 21:36:36 +0100

    solve event, and this, in html, no same when is by window

--- app/js/base/vera.js
+++ app/js/base/vera.js
@@ -15,11 +15,11 @@ var listener = (function(){
     function remove(){
 
     }
-    function dispatch(name){
+    function dispatch(name, arguments){
       if(typeof(listeners[name])!='undefined'){
         var i=0, l=listeners[name].length;
         for(i;i<l;i++)
-          listeners[name][i][0].apply(null,listeners[name][i][1]);
+          listeners[name][i][0].apply(listeners[name][i][1], arguments);
       }
     }
 })();
@@ -29,10 +29,10 @@ var listener = (function(){
 
 var route = (function(listener){
   var paths = [];
-  var ___i = 0;
+  var event, el;
   ////if(listener) listener.add('ON_PATH', update, ['pepe', 'paco']);
   __init();
-  console.log('inicio la app');
+
 
 
   var app ={
@@ -44,50 +44,66 @@ var route = (function(listener){
       console.log('rest');
     }
   }
-  return function(path){
-    return (__set(path)) ? app : false;
+  return function(){
+    var args = Array.prototype.slice.call(arguments);
+    var _path = args.slice(-1)[0];
+    console.log(args.length, args);
+
+    event = __getMouseEvent(args);
+    el = __getThis(args);
+
+    return (__set(_path)) ? app : false;
+  }
+  function __getMouseEvent(args){
+    var i = 0, l = args.length;
+    for(i;i<l;i++)
+      if(args[i]!==null && args[i].currentTarget) return args[i];
+    return null;
   }
 
-  function __apply(path){
-
+  function __getThis(args){
+    var i = 0, l = args.length;
+    for(i;i<l;i++)
+      if(args[i]!==null && args[i].innerHTML) return args[i];
+    return null;
   }
 
   function __applied(path){
     return (window.location.hash.lastIndexOf(path)>-1);
   }
+
   function __set(path)
   {
     if(!__applied(path)){
-
       window.location.hash = __cleanPath(path);
       return true;
     }
     return false;
   }
-
   function __cleanPath(path){
     return (/^\#/.test(path)) ? path.split('#')[1] : path;
   }
   function __init()
   {
-    console.log('CALLED: TIMES!!!!');
     if (window.addEventListener)
       window.addEventListener('hashchange', __onChange, false);
     else if (window.attachEvent)
       window.attachEvent('hashchange', __onChange);
-
+    return true;
   }
-
-
   function set(path){
-
   }
-
   function __onChange(){
-    ___i++;
-    console.log(___i, ' CALLED =====');
-    console.log('update from base', arguments);
+    if(listener)
+      listener.dispatch('HASH_CHAGE', [event, el, window.location.hash]);
+    return paths;
   }
 })(listener);
-var r = route('#home');
+
+var r = route(null, '#home');
+
+listener.add('HASH_CHAGE', function(e, el, hash){
+  var elm = document.getElementById('test-route');
+  elm.innerHTML+= '<p>site link actual: '+hash+'</p>';
+});
 console.log(r, 'pp');
