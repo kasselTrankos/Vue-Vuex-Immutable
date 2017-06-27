f4feba791d9d3436eebb5c8091d1f35499588481
kasselTrankos <alvaro.touzon@gmail.com>
2017-06-27 12:31:23 +0200

    activacion de checkbos, very sencilla

--- app/vue/components/myComponent.vue
+++ app/vue/components/myComponent.vue
@@ -7,7 +7,13 @@
 <template>
 	<div class="row">
     	<h1 class="col6 red  col-md-offset-4">{{ msg }}!!!!</h1>
-
+    	<form class="col4 col-md-offset-6">
+    		<div class="checkbox">
+		    <label>
+		      <input type="checkbox" v-model="checked"> {{title}}
+		    </label>
+		  </div>
+    	</form>
     </div>
 </template>
 
@@ -16,7 +22,14 @@
 export default {
   data () {
     return {
-      msg: 'Mi componente'
+      msg: 'Mi componente',
+      checked: true,
+      title: 'Check me'
+    }
+  },
+  methods: {
+    check() {
+      this.checked = !this.checked;
     }
   }
 }
