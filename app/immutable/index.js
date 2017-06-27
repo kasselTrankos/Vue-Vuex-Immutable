cec70df5967c2d0e40f031f9a978b4b21a34222d
kasselTrankos <alvaro.touzon@gmail.com>
2017-06-27 09:02:20 +0200

    using map

--- app/immutable/index.js
+++ app/immutable/index.js
@@ -1,5 +1,6 @@
-import { Map } from "immutable";
-const map1 = Map({ a: 1, b: 2, c: 3 });
-const map2 = map1.set('b', 50);
-map1.get('b'); // 2
-map2.get('b'); // 50
+const { Map } = require('immutable')
+const map1 = Map({ a: 1, b: 2, c: 32 });
+
+console.log(map1.get('c'), 'MAP from immutable');
+
+
